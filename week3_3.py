6229564cc24559b20c43e0be5a305c1439f64fc4
Lwen1243 <1198807618@qq.com>
2022-03-09 00:38:17 +0800

    第三周第三题（半成品）

--- week3_3.py
+++ week3_3.py
@@ -7,10 +7,11 @@ print(prpr)
 # 查询列索引为two和three两列数据
 print(prpr.loc[:, ['one', 'three']])
 # 查询第0行、第2行、第0列、第2列数据
-print(prpr.loc[0, 2])
-print(prpr.loc[:, [0, 2]])
+print(prpr.iloc[[0, 2], :])
+print(prpr.iloc[:, [0, 2]])
 # 筛选第1列中值大于2的所有行数据，保存到data1对象
-
+data1 = prpr.iloc[:, 1]
+print(data1)
 # 为data1添加一列数据，列索引为four，值都为10
 
 # 将data1所有值大于9的数据修改为8
